3cd9956e33295818412d8e267a1d05d6b9bf8590
dylanPMurphy <dylanmurphydba@gmail.com>
2020-09-22 12:37:50 -0700

    /index /create /new

--- firstDjangoProject/core_app/views.py
+++ firstDjangoProject/core_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Placeholder to display list of blogs")
+
+def new(request):
+    return HttpResponse("Placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect('/')
